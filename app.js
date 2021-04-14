cb0bbf8ccceaaeb22952e45ed90c8f1928ac4779
kamataryo <kamataryo@users.noreply.github.com>
2021-04-14 11:49:23 +0900

    Fix error

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ layout: compress
 
 /* Registering Service Worker */
 if('serviceWorker' in navigator) {
-  // navigator.serviceWorker.register('{{ "/sw.js" | relative_url }}');
+  /* navigator.serviceWorker.register('{{ "/sw.js" | relative_url }}'); */
 };
